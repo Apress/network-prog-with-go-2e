64f2475e46fcf60e6b7deda16b4a144dc0f43ecb
Ken Stone Blue <ken@stoneblue.com>
2022-08-13 16:35:03 -0500

    Change Fatalln to Fatalf

--- ch3/lookuphost.go
+++ ch3/lookuphost.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	if len(os.Args) != 2 {
-		log.Fatalln("Usage: %s hostname\n", os.Args[0])
+		log.Fatalf("Usage: %s hostname\n", os.Args[0])
 	}
 	name := os.Args[1]
 	cname, _ := net.LookupCNAME(name)
